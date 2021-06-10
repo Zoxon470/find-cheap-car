0a8a4f248988b5f47dcdc0491db40c5f1feedd4b
Ahmed Asadov <zoxon470@gmail.com>
2021-06-10 06:27:25 +0600

    Add models Brand, Model to alembic/env.py

--- alembic/env.py
+++ alembic/env.py
@@ -6,6 +6,7 @@ from alembic import context
 from core.config import settings
 from database.config import Base, engine
 from models.user import BaseModel, User
+from models.car import Brand, Model
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -26,7 +27,7 @@ target_metadata = Base.metadata
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
-_ = [BaseModel, User]
+_ = [BaseModel, User, Brand, Model]
 
 
 def run_migrations_offline():
