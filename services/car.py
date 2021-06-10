66eb49c2be266e99ba845a77f40924a65f290790
Ahmed Asadov <zoxon470@gmail.com>
2021-06-10 06:26:17 +0600

    Add BrandService, ModelService

---
+++ services/car.py
@@ -0,0 +1,36 @@
+from sqlalchemy import select
+
+from models.car import Brand, Model
+from services.base import DBSessionContext
+
+
+class BrandService(DBSessionContext):
+    model = Brand
+
+    async def get_brand(self, brand_id: int):
+        result = await self.db.execute(
+            select(self.model).filter(self.model.id == brand_id)
+        )
+        return result.one()
+
+    async def get_brands(self):
+        result = await self.db.execute(
+            select(self.model)
+        )
+        return result.scalars().all()
+
+
+class ModelService(DBSessionContext):
+    model = Model
+
+    async def get_model(self, model_id: int):
+        result = await self.db.execute(
+            select(self.model).filter(self.model.id == model_id)
+        )
+        return result.one()
+
+    async def get_models(self, brand_id: int):
+        result = await self.db.execute(
+            select(self.model).filter_by(brand_id=brand_id)
+        )
+        return result.scalars().all()
