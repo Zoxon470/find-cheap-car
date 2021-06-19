0cf7993cf2c29f22683925a88c07a7475b22cbd1
Zoxon470 <zoxon470@gmail.com>
2021-06-19 13:36:50 +0600

    Add class AbstractProvider

---
+++ providers/__init__.py
@@ -0,0 +1,10 @@
+from .mashina_kg import MashinaKGProvider
+
+mashina_kg = MashinaKGProvider(
+    name="MashinaKG",
+    url="https://www.mashina.kg"
+)
+
+providers = [
+    mashina_kg
+]
--- providers/abstract.py
+++ providers/abstract.py
@@ -1,15 +1,21 @@
 from httpx import AsyncClient
+from pydantic import HttpUrl
 
-from pydantic import AnyHttpUrl
+from schemas.search import Query, SearchResult
 
 
 class AbstractProvider:
-    def __init__(self, http_client: AsyncClient, name: str, url: AnyHttpUrl):
-        self.http_client = http_client
+    def __init__(
+            self,
+            name: str,
+            url: HttpUrl,
+            http_client: AsyncClient = AsyncClient
+    ):
         self.name = name
         self.url = url
+        self.http_client = http_client
 
-    async def job(self, query: str): ...
+    async def job(self, query: Query): ...
 
-    async def result(self, *args, **kwargs):
-        pass
+    def result(self, **kwargs) -> SearchResult:
+        return SearchResult(**kwargs)
