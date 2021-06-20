1f99ed93306dfa168e4fd9d72bd842423b91a430
Zoxon470 <zoxon470@gmail.com>
2021-06-20 10:18:17 +0600

    Add method get_validated_price to class MashinaKGProvider

--- providers/mashina_kg.py
+++ providers/mashina_kg.py
@@ -1,11 +1,18 @@
+from typing import Union
+
 import httpx
 import parsel
 
-from .abstract import AbstractProvider
 from schemas.search import Query
+from .abstract import AbstractProvider
 
 
 class MashinaKGProvider(AbstractProvider):
+    def get_validated_price(self, value: str) -> Union[str, None]:
+        if value:
+            return value.replace(" ", "")[1:]
+        return None
+
     async def job(self, query: Query):
         search_url = f"{self.url}/search/{query.brand.lower()}/{query.model.lower()}"
         async with httpx.AsyncClient() as client:
@@ -14,6 +21,10 @@ class MashinaKGProvider(AbstractProvider):
 
             return list(map(lambda x: self.result(
                 url=f"{self.url}{x.xpath('@href').get()}",
-                price=x.css("div > p > strong::text").get(),
-                image=x.css(".thumb-item-carousel")[0].css("img")[0].xpath("@data-src").get()
+                price=self.get_validated_price(
+                    x.css("div > p > strong::text").get()
+                ),
+                image=x.css(
+                    ".thumb-item-carousel"
+                )[0].css("img")[0].xpath("@data-src").get()
             ), html.css(".table-view-list .list-item > a")))
